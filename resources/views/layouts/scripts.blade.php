3f61de2cbcc0f14bc96c252636fc0304d5e1a089
Lizä Hörnwood <lizahornwood@gmail.com>
2016-07-08 23:30:47 -0500

    Sweetalert

---
+++ resources/views/layouts/scripts.blade.php
@@ -0,0 +1,53 @@
+@section('scripts')
+
+<script type="text/javascript">
+
+$('.btn-danger').click(function()
+	{
+		var form1= $(this).closest("form");
+
+		swal({
+			    title: 'Está seguro que desea eliminarlo?',
+				text: "",
+				type: 'warning',
+				showCancelButton: true,
+				confirmButtonColor: '#3085d6',
+				cancelButtonColor: '#d33',
+				cancelButtonText:"Cancelar",
+				confirmButtonText: 'Sí, eliminarlo!'
+			 }).then(function() 
+			 {
+			 	form1.submit();
+
+				 swal(
+			     'Eliminado!',
+			     'Ha sido eliminado correctamente.',
+			     'success'
+				   );
+			 });
+
+	 });
+
+
+
+	var message = "{{Session::get('message')}}"
+
+		if ("{{Session::has('message')}}") 
+		 {
+		 	swal(
+				  message,
+				  '',
+				  'success'
+                 )
+		 }
+
+
+    $("#idDP").datepicker(
+    {
+    	format: "yyyy/mm/dd",
+    	language: "es"
+    })
+
+</script>
+
+@stop
